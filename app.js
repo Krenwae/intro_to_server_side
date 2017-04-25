8b9c7512a11d55c7cec894a30ac31593c9e76bc6
Krenwae <spencer@spencersvoice.com>
2017-04-24 21:07:17 -0600

    Server side programming for weblab
    Day 1 work for server side programming. Nope, express, etc

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+var port= 5000;
+
+app.use(express.static('public'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+var locals = {
+  title: "Hello from Express",
+  date: new Date(),
+  list: ['a', 'b', 'c']
+};
+
+app.get('/', function(req, res){
+  res.render("index", locals);
+});
+
+app.listen(port, function(err){
+  console.log("Listening on port: " + port)
+})

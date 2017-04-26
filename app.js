87159696279c19bda61ca08287279040e684c91e
Krenwae <spencer@spencersvoice.com>
2017-04-25 18:58:15 -0600

    Day 2
    Some gulp stuff(which is better than the debug), and then we did a bit
    of app.get. I don't really know what that's called.

--- app.js
+++ app.js
@@ -18,6 +18,35 @@ app.get('/', function(req, res){
   res.render("index", locals);
 });
 
+var users=[
+  {
+    firstName: "Spencer",
+    lastName: "Sholander"
+  },
+  {
+    firstName: "Nick",
+    lastName: "Cappasso"
+  },
+  {
+    firstName: "Ian",
+    lastName: "Sorensen"
+  },
+  {
+    firstName: "Greatest",
+    lastName: "Ever"
+  }
+];
+
+app.get('/users', function(req, res){
+ res.send(users);
+});
+
+app.get('/users/:id', function(req, res){
+  console.log(req.params);
+  var user = users[req.params.id];
+  res.send(user);
+});
+
 app.listen(port, function(err){
   console.log("Listening on port: " + port)
-})
+});
